7603bc51356fa797443c1fec789145e8d39bb499
Mozart <james.hiraldo001@mymdc.net>
2025-08-11 18:17:11 -0400

    New update , changing file to cpp
    filE WAS CHANGED TO CPP

---
+++ Ouija Board display.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// First project: Ouija Board display
+
+int main() {
+  std::cout <<"----------------------------- \n";
+  std::cout << "|       x YES     NO x      |\n";
+  std::cout << "|                           |\n";
+  std::cout << "|      ABCDEFGHIJKLM        |\n";
+  std::cout << "|      NOPQRSTUvWXYZ        |\n";
+  std::cout << "|                           |\n";
+  std::cout << "|        1234567890         |\n";
+  std::cout << "----------------------------- \n";
+
+}
